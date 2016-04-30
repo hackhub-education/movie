8d8ae20376dcea1e4953f50f9734998f480c50dc
Yan Hong <onlyhongyan@gmail.com>
2016-04-30 15:41:16 -0700

    add ticket left row

--- js/ticket.js
+++ js/ticket.js
@@ -22,29 +22,30 @@ var movies = [
     price: 4,
     avaliable: false,
     ticketLeft: 5
+  },
+  {
+    id: 'm3',
+    date: '1978-05-02',
+    name: 'Go',
+    price: 17,
+    avaliable: true,
+    ticketLeft: 5
   }
 ];
 
 for (var i = 0; i < movies.length; i++) {
-
-
   var dateTd = '<td>' + movies[i].date + '</td>';
   var nameTd = '<td>' + movies[i].name + '</td>';
   var priceTd = '<td>' + movies[i].price + '</td>';
-
+  var ticketTd = '<td id="t' + movies[i].id +'">' + movies[i].ticketLeft + '</td>';
   if (movies[i].avaliable && movies[i].ticketLeft > 0) {
-
     // movie is avaliable and has ticket left, user can purchase
-
     var purchaseTd = '<td><button class="btn btn-primary btn-purchase" id="'+ movies[i].id + '">Purchase</button></td>';
   } else {
-
     // movie is not avaliable and user cannot purchase
-
     var purchaseTd = '<td><button class="btn" disabled="true">Not Avaliable</button></td>';
   }
-
-  $('<tr>').html(dateTd + nameTd + priceTd + purchaseTd).appendTo('#ticket-table');
+  $('<tr>').html(dateTd + nameTd + priceTd + purchaseTd + ticketTd).appendTo('#ticket-table');
 }
 
 $('.btn-purchase').click(function(e) {
@@ -52,7 +53,7 @@ $('.btn-purchase').click(function(e) {
     if (movies[i].id === e.target.id) {
       if (movies[i].ticketLeft > 0) {
         movies[i].ticketLeft--;
-        console.log(movies[i].ticketLeft);
+        $('#t' + movies[i].id).text(movies[i].ticketLeft);
       } 
       if (movies[i].ticketLeft < 1) {
         // $(e.target).text('Not Avaliable').css({
@@ -64,9 +65,3 @@ $('.btn-purchase').click(function(e) {
     }
   }
 });
-
-
-
-
-
-
