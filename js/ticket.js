42e5b0023748faa391c59815ed2171f0072112c5
Yan Hong <onlyhongyan@gmail.com>
2016-05-01 13:46:21 -0700

    link firebase

--- js/ticket.js
+++ js/ticket.js
@@ -1,52 +1,70 @@
-var movies = [
-  {
-    id: 'm0',
-    date: '2015-10-25',
-    name: 'Avengers',
-    price: 10,
-    avaliable: true,
-    ticketLeft: 5
-  }, 
-  {
-    id: 'm1',
-    date: '2016-01-21',
-    name: 'Start Wars',
-    price: 12,
-    avaliable: true,
-    ticketLeft: 2
-  },
-  {
-    id: 'm2',
-    date: '1998-05-02',
-    name: 'Titanic',
-    price: 4,
-    avaliable: false,
-    ticketLeft: 5
-  },
-  {
-    id: 'm3',
-    date: '1978-05-02',
-    name: 'Go',
-    price: 17,
-    avaliable: true,
-    ticketLeft: 5
-  }
-];
+// var movies = [
+//   {
+//     id: 'm0',
+//     date: '2015-10-25',
+//     name: 'Avengers',
+//     price: 10,
+//     avaliable: true,
+//     ticketLeft: 5
+//   }, 
+//   {
+//     id: 'm1',
+//     date: '2016-01-21',
+//     name: 'Start Wars',
+//     price: 12,
+//     avaliable: true,
+//     ticketLeft: 2
+//   },
+//   {
+//     id: 'm2',
+//     date: '1998-05-02',
+//     name: 'Titanic',
+//     price: 4,
+//     avaliable: false,
+//     ticketLeft: 5
+//   },
+//   {
+//     id: 'm3',
+//     date: '1978-05-02',
+//     name: 'Go',
+//     price: 17,
+//     avaliable: true,
+//     ticketLeft: 5
+//   }
+// ];
 
-for (var i = 0; i < movies.length; i++) {
-  var dateTd = '<td>' + movies[i].date + '</td>';
-  var nameTd = '<td>' + movies[i].name + '</td>';
-  var priceTd = '<td>' + movies[i].price + '</td>';
-  var ticketTd = '<td id="t' + movies[i].id +'">' + movies[i].ticketLeft + '</td>';
-  if (movies[i].avaliable && movies[i].ticketLeft > 0) {
-    // movie is avaliable and has ticket left, user can purchase
-    var purchaseTd = '<td><button class="btn btn-primary btn-purchase" id="'+ movies[i].id + '">Purchase</button></td>';
-  } else {
-    // movie is not avaliable and user cannot purchase
-    var purchaseTd = '<td><button class="btn" disabled="true">Not Avaliable</button></td>';
+var movies;
+
+
+var movieDB = new Firebase('https://burning-inferno-1270.firebaseio.com/');
+
+// movieDB.set(movies);
+
+movieDB.on('value', function(snapshot) {
+
+  console.log(snapshot.val());
+
+  $('.movie-row').remove();
+
+  movies = snapshot.val();
+
+
+  for (var i = 0; i < movies.length; i++) {
+    var dateTd = '<td>' + movies[i].date + '</td>';
+    var nameTd = '<td>' + movies[i].name + '</td>';
+    var priceTd = '<td>' + movies[i].price + '</td>';
+    var ticketTd = '<td id="t' + movies[i].id +'">' + movies[i].ticketLeft + '</td>';
+    if (movies[i].avaliable && movies[i].ticketLeft > 0) {
+      // movie is avaliable and has ticket left, user can purchase
+      var purchaseTd = '<td><button class="btn btn-primary btn-purchase" id="'+ movies[i].id + '">Purchase</button></td>';
+    } else {
+      // movie is not avaliable and user cannot purchase
+      var purchaseTd = '<td><button class="btn" disabled="true">Not Avaliable</button></td>';
+    }
+    $('<tr>').addClass('movie-row').html(dateTd + nameTd + priceTd + purchaseTd + ticketTd).appendTo('#ticket-table');
   }
-  $('<tr>').html(dateTd + nameTd + priceTd + purchaseTd + ticketTd).appendTo('#ticket-table');
-}
+
+});
 
 $('.btn-purchase').click(function(e) {
 
